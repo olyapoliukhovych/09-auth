bf0881546167c2d5e5edb01a8456dc041aa4936c
olya <olha.poliukhovych.1@gmail.com>
2026-01-20 11:02:56 +0200

    Remove interceptors

--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -1,7 +1,6 @@
 import { User } from "@/types/user";
 import { nextServer } from "./api";
 import { Tag } from "@/components/NoteForm/NoteForm";
-import toast from "react-hot-toast";
 import { Note, NoteTag } from "@/types/note";
 
 export interface FetchNotesResponse {
@@ -101,53 +100,4 @@ export const getMe = async () => {
 export const updateMe = async (data: { username: string }) => {
   const res = await nextServer.patch("/users/me", data);
   return res.data;
-};
-
-// push notifications
-nextServer.interceptors.response.use(
-  (response) => {
-    const excludedUrls = ["/auth/login", "/auth/logout", "/auth/register"];
-
-    const isExcluded = excludedUrls.some((url) =>
-      response.config.url?.includes(url),
-    );
-
-    if (
-      ["patch", "put", "post"].includes(response.config.method || "") &&
-      !isExcluded
-    ) {
-      toast.success("Changes saved successfully!");
-    }
-    return response;
-  },
-  (error) => {
-    const requestUrl = error.config?.url || "";
-    const status = error.response?.status;
-    const message =
-      error.response?.data?.message || error.message || "Something went wrong";
-
-    const isSilentEndpoint = requestUrl.includes("/users/me");
-    if (isSilentEndpoint) {
-      return Promise.reject(error);
-    }
-
-    let finalMessage = message;
-
-    if (status === 400) finalMessage = `Validation Error: ${message}`;
-    else if (status === 403) finalMessage = "Access denied!";
-    else if (status === 404) finalMessage = "Resource not found";
-    else if (status === 500) finalMessage = "Server error, try again later";
-
-    toast.error(finalMessage);
-
-    return Promise.reject(error);
-  },
-);
-
-export const notify = {
-  loginSuccess: () => toast.success("Logged in successfully"),
-
-  logoutSuccess: () => toast.success("Logged out successfully"),
-
-  registerSuccess: () => toast.success("Account created!"),
 };
